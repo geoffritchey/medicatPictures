c26270b6617a2217eac56f7d4f8f7ad054db9e6f
Geoffrey Ritchey <Geoffrey.Ritchey@gmail.com>
2021-08-18 13:57:48 -0500

    automatically upload newest pictures

--- CopyFile.py
+++ CopyFile.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 from PIL import Image
 import cv2
-import ftplib
+import pysftp
 import build
 import time
 import json
@@ -11,28 +11,14 @@ import ssl
 import logging
 import Util
 
-
 logging.basicConfig(format='%(message)s')
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-send = False
+send = True
 max_modified_time = 0
 
 properties = dict()
-
-
-def storbinary(ftp, cmd, fp, blocksize=8192, callback=None, rest=None):
-    ftp.voidcmd('TYPE I')
-    with ftp.transfercmd(cmd, rest) as conn:
-        while 1:
-            buf = fp.read(blocksize)
-            if not buf: break
-            conn.sendall(buf)
-            if callback: callback(buf)
-        if isinstance(conn, ssl.SSLSocket):
-            pass
-    return ftp.voidresp()
 
 
 def get_current_students(session=Util.authenticated_session, today=datetime.datetime.now()):
@@ -65,15 +51,29 @@ def get_current_students(session=Util.authenticated_session, today=datetime.date
     # start period list as a set to get distinct values
     # then convert it to a list for easy of use
 
-    dict_numbers = dict()
+    id_to_picture_file_mapping = dict()
     for child in result.get("value"):
         all_id_numbers.add(child["Student"]["StudentNumber"])
-        dict_numbers[child["Student"]["StudentNumber"]] = child["Student"]["Id"]
+        id_to_picture_file_mapping[child["Student"]["StudentNumber"]] = child["Student"]["Id"]
 
-    return all_id_numbers, dict_numbers
+    return all_id_numbers, id_to_picture_file_mapping
 
 
 if __name__ == '__main__':
+    new_files_to_upload = []
+    try:
+        with open('properties.txt') as f:
+            properties = json.load(f)
+    except json.JSONDecodeError as err:
+        print(err)
+
+    try:
+        str_last_modify = properties['last_modified']
+    except:
+        str_last_modify = "1900-01-01 00:00:00"
+
+    print("last_modify = ", str_last_modify)
+    last_modify_time = datetime.datetime.strptime(str_last_modify, "%Y-%m-%d %H:%M:%S").timestamp()
 
     upload_files, mappings = get_current_students()
 
@@ -81,39 +81,40 @@ if __name__ == '__main__':
     for p in pathlib.Path('//lcuops/images$').glob("*.jpg"):
         if p.is_file():
             if p.stem in upload_files:
-                print("found: ", p)
-                image = cv2.imread(str(p))
-                resize = cv2.resize(image, (63, 73))
-                cv2.imwrite("./resized/" + p.name, resize)
                 other_id = p.stem
                 patient_control_id = mappings[other_id]
                 print(f"{patient_control_id if patient_control_id is not None else ''}"
                       , f"{other_id[:20] if other_id is not None else ''}"
                       , sep="|", file=f)
-            else:
-                print("not found: ", p)
+                modified = int(os.path.getmtime(p))
+                if modified > last_modify_time:
+                    print("found: ", p)
+                    image = cv2.imread(str(p))
+                    resize = cv2.resize(image, (63, 73))
+                    cv2.imwrite("./resized/" + p.name, resize)
+                    new_files_to_upload.append(p.name)
+                if modified > max_modified_time:
+                    max_modified_time = modified
     f.close
 
-'''
-Pictures were manually uploaded.  An automated upload has not been written yet.
-
-The Automatic upload below is a clone of nexus CopyPicture, which will need modifications for medicat
-
     if send:
-        if len(upload_files) > 0:
-            ftp = ftplib.FTP_TLS(build.sftp_host)
-            ftp.login(build.sftp_user, build.sftp_password)
-            ftp.prot_p()
-            ftp.cwd("/Production/StudentPicureImport")
-            print("LIST CWD = ", ftp.pwd())
-            print(ftp.retrlines("LIST"))
-            for filename in upload_files:
-                command = 'STOR ' + filename.name
-                print(command, str(filename))
-                print('uploading ' + filename.name)
-                storbinary(ftp, 'STOR ' + filename.name, open(str(filename), 'rb'))
-                print('uploading done ' + filename.name)
-            print("LIST CWD = ", ftp.pwd())
-            print(ftp.retrlines("LIST"))
+        if len(new_files_to_upload) > 0:
+            os.chdir("resized")
+            ftp = pysftp.Connection(build.sftp_host, username=build.sftp_username, password=build.sftp_password)
+            ftp.cwd("1376Photos")
+            print("LIST CWD = ", ftp.pwd)
+            for filename in new_files_to_upload:
+                print('uploading ' + filename)
+                ftp.put(filename)
+                print('uploading done ' + filename)
+            print("LIST CWD = ", ftp.pwd)
             ftp.close()
-'''
+
+            time = datetime.datetime.strptime(time.ctime(max_modified_time), "%a %b %d %H:%M:%S %Y").strftime(
+                "%Y-%m-%d %H:%M:%S")
+            print(time)
+            properties['last_modified'] = time
+            print(properties)
+            os.chdir("..")
+            with open('properties.txt', 'w+') as f:
+                json.dump(properties, f)
